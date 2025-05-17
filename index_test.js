42106ecaa820f4a661fdf81cf1b166285c2e210a
Jocy5816U <148248231+Jocy5816U@users.noreply.github.com>
2025-05-17 11:13:03 -0600

    Initial commit

---
+++ index_test.js
@@ -0,0 +1,5 @@
+const { suma } = require('./index');
+
+console.log(suma(3, 4)); // Debería imprimir 7
+console.log(suma(5, 6)); // Debería imprimir 11
+console.log(suma(10, 20)); // Debería imprimir 30
